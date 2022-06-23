07d64965a62a47811ac69e2a518c1041d423b144
Kristina <87947917+KrisssMi@users.noreply.github.com>
2022-06-23 15:20:24 +0300

    ADD

---
+++ Лабораторная №9/Lab_9/Lab_9/Phone.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab_9
+{
+    public class Phone : DependencyObject
+    {
+        public static readonly DependencyProperty TitleProperty;
+        public static readonly DependencyProperty PriceProperty;
+
+
+        static Phone()
+        {
+            TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Phone));    // регистрирует свойство зависимостей с указанием имени свойства, типа свойства и типа владельца
+
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.CoerceValueCallback = new CoerceValueCallback(CorrectValue);
+            PriceProperty = DependencyProperty.Register("Price", typeof(int), typeof(Phone), metadata, new ValidateValueCallback(ValidateValue));       // регистрирует свойство зависимостей с указанием имени свойства, типа свойства и типа владельца
+
+        }
+
+        private static bool ValidateValue(object value)
+        {
+            int currentValue = (int)value;
+
+            if (currentValue >= 0)
+                return true;
+            return false;
+        }
+
+        private static object CorrectValue(DependencyObject dependencyObject, object value)
+        {
+            int currentValue = (int)value;
+
+            if (currentValue > 10000)
+                return MessageBox.Show("Слишком дорого!");
+            else 
+                return currentValue;
+        }
+
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public int Price
+        {
+            get { return (int)GetValue(PriceProperty); }
+            set { SetValue(PriceProperty, value); }
+        }
+    }
+}
---
+++ Лабораторная №9/Lab_9/Lab_9/UserControl1.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_9
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public object Secret
+        {
+            get { return (object)GetValue(SecretProperty); }
+            set { SetValue(SecretProperty, value); }
+        } 
+
+        public static readonly DependencyProperty SecretProperty = DependencyProperty.Register("Secret", typeof(object), typeof(UserControl1), new PropertyMetadata("UserControl Visible"));
+
+
+        private void Spoiler_Click(object sender, RoutedEventArgs e)
+        {
+            if (spoilerGrid.Visibility == Visibility.Visible)
+            {
+                contentGrid.Visibility = Visibility.Visible;
+                spoilerGrid.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                contentGrid.Visibility = Visibility.Collapsed;
+                spoilerGrid.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
